bc33c70e4d59eacda4fa68e8fddd9e870d66c517
MBKenn <teerapat.yj@gmail.com>
2023-03-14 19:24:41 +0700

    Update split

--- ft_split.c
+++ ft_split.c
@@ -11,41 +11,83 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stddef.h>
 
-static	int	count_word(char const *s, char c)
+static	size_t	count_word(char const *s, char c)
 {
-	int	count;
-	int	i;
+	size_t	count;
+	size_t	i;
 
 	count = 0;
-	i = 1;
+	i = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
-		{
-			if (s[i+1] == c)
-			{
-				count++;
-			}
-		}
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
+			count++;
+		while (s[i] && (s[i] != c))
+			i++;
+	}
+	return count;	
+}
+
+static char	**free_array(char **arr, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		free(arr[i]);
 		i++;
+	}
+	free(arr);
+	return NULL;
+}
+
+static char	*cpy_word(char const *s, char c)
+{
+	size_t	len;
+	char	*word;
+
+	len = 0;
+	while (*s && *s != c)
+	{
+		len++;
 		s++;
 	}
-	return count;
-	
+	word = malloc(sizeof(char) * (len + 1));
+	if (!word)
+		return (NULL);
+	ft_strlcpy(word, s - len, len + 1);
+	return (word);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int a = count_word(*s, c);
-	return ;
+	size_t	i;
+	size_t	word;
+	char	**arr_str;
+	
+	if (!s)
+		return NULL;
+	word = count_word(s, c);
+	arr_str = malloc(sizeof(char *) * (word + 1));
+	if (!arr_str)
+		return NULL;
+	i = 0;
+	arr_str[word] = NULL;
+	while (i < word)
+	{
+		while (*s && *s == c)
+			s++;
+		arr_str[i] = cpy_word(s, c);
+		if (!arr_str)
+			return (free_array(arr_str,i));
+		while (*s && *s != c) 
+			s++;
+		i++;
+	}
+	return (arr_str);
 }
 
-int main()
-{
-	char s = "sadfgasfd";
-	int a = count_word(s,a);
-	printf("a : \n",a);
-	return 0;
-}
